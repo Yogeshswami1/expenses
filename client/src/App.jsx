a06cf30656f93fe252f0372db3bfadccd4437974
Yogesh <yogeshkumar86077@gmail.com>
2025-07-17 22:14:07 +0530

    nn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -93,7 +93,7 @@ function App() {
   return (
     <div className="wrapper">
       <div className="tracker-box">
-        <h1>Expeense Yogesh Tracker hanji💰</h1>
+        <h1>Expeense Yogesh Bhai kya haal chalTracker hanji💰</h1>
 
         <div className="form">
           <input
